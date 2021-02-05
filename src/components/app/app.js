2247032434b7e1209a660392612633aaa502cfba
a.zubov <zubov.alexey@gmail.com>
2021-02-05 17:29:53 +0300

    App as func component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
@@ -9,11 +8,31 @@ import './app.css';
 
 const App = () => {
 
-  const todoData = [
+  const [todoData, setTodoData] = useState([
     { label: 'Drink Coffee', important: false, id: 1 },
     { label: 'Make Awesome App', important: true, id: 2 },
     { label: 'Have a lunch', important: false, id: 3 }
-  ];
+  ])
+
+  // const todoData = [
+  //   { label: 'Drink Coffee', important: false, id: 1 },
+  //   { label: 'Make Awesome App', important: true, id: 2 },
+  //   { label: 'Have a lunch', important: false, id: 3 }
+  // ];
+
+  // const deleteItem = (id) => {
+  //   setTodoData( (todoData) => {
+  //     const idx = todoData.findIndex((el) => el.id === id)
+  //     todoData.splice(idx, 1)
+  //     return {
+  //       todoData: todoData
+  //     }
+  //   })
+  // }
+
+  const deleteItem = (id) => {
+    setTodoData(todoData.filter(el => el.id !== id))
+    }
 
   return (
     <div className="todo-app">
@@ -23,7 +42,8 @@ const App = () => {
         <ItemStatusFilter />
       </div>
 
-      <TodoList todos={todoData} />
+      <TodoList todos={todoData}
+        onDeleted={ deleteItem} />
     </div>
   );
 };
