a1038fcf889f8165e61c5af2df4a26c84b6b6d4a
a.zubov <zubov.alexey@gmail.com>
2021-02-05 18:19:31 +0300

    add addItem class

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,51 +1,60 @@
-import React, {useState} from 'react';
+import React from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
+import ItemAddForm from '../item-add-form';
 
-const App = () => {
+export default class App extends React.Component {
+  maxId = 100;
 
-  const [todoData, setTodoData] = useState([
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ])
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 },
+    ],
+  };
 
-  // const todoData = [
-  //   { label: 'Drink Coffee', important: false, id: 1 },
-  //   { label: 'Make Awesome App', important: true, id: 2 },
-  //   { label: 'Have a lunch', important: false, id: 3 }
-  // ];
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+      const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+      return {
+        todoData: newArray,
+      };
+    });
+  };
 
-  // const deleteItem = (id) => {
-  //   setTodoData( (todoData) => {
-  //     const idx = todoData.findIndex((el) => el.id === id)
-  //     todoData.splice(idx, 1)
-  //     return {
-  //       todoData: todoData
-  //     }
-  //   })
-  // }
+  addItem = (text) => {
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++,
+    };
 
-  const deleteItem = (id) => {
-    setTodoData(todoData.filter(el => el.id !== id))
-    }
+    this.setState(({ todoData }) => {
+      const newArray = [...todoData, newItem];
+      return {
+        todoData: newArray,
+      };
+    });
+  };
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  render() {
+    return (
+      <div className='todo-app'>
+        <AppHeader toDo={1} done={3} />
+        <div className='top-panel d-flex'>
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
+        <ItemAddForm onItemAdded={this.addItem} />
       </div>
-
-      <TodoList todos={todoData}
-        onDeleted={ deleteItem} />
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
